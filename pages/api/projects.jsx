ee3d19e86e97277d661a2ecceeeb3b06cab2b156
iRianPeekBtw31 <95495922+iRianPeekBtw31@users.noreply.github.com>
2023-06-30 00:43:04 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,14 +2,14 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://cdn.discordapp.com/attachments/1117809170519961703/1124055117562396763/uajY0XrMkYM-removebg-preview.png",
+      image: "https://cdn.discordapp.com/attachments/1117809170519961703/1124092152864051220/JPEG_20190128_230712.jpg",
       name: "Game House",
       description: "Discord türkiyenin en iyi oyun ortamı",
       link: "https://discord.gg/Game House",
     },
     {
       id: 2,
-      image: "https://cdn.discordapp.com/attachments/1117809170519961703/1124065680627613796/JPEG_20180902_205240.jpg",
+      image: "https://cdn.discordapp.com/attachments/1117809170519961703/1124092133738025110/JPEG_20190128_232356.jpg",
       name: "Youtube Channel",
       description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
       link: "https://www.youtube.com/@irian35",
