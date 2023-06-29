e6c629b9520e38151a0d0dc0df0175d8da9a8866
iRianPeekBtw31 <95495922+iRianPeekBtw31@users.noreply.github.com>
2023-06-29 22:00:35 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const navItems = [
     { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
     { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
     { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
-    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.umut.live' }
+    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://www.irian.xyz' }
 ]
 
 import Link from 'next/link';
