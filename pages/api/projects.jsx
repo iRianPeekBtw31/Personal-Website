c7714a8d4a110ceb634eb51c97349ecdcde6fe17
iRianPeekBtw31 <95495922+iRianPeekBtw31@users.noreply.github.com>
2023-06-29 22:15:47 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -1,6 +1,6 @@
 
     {
-      id: 2,
+      id: 1,
       image: "https://cdn.discordapp.com/attachments/1117809170519961703/1124055117562396763/uajY0XrMkYM-removebg-preview.png",
       name: "Youtube Channel",
       description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
