6ccdd26d92c23654c196e52c752b5fb662379868
iRianPeekBtw31 <95495922+iRianPeekBtw31@users.noreply.github.com>
2023-06-29 22:56:58 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -3,9 +3,17 @@ export default async (req, res) => {
     {
       id: 1,
       image: "https://cdn.discordapp.com/attachments/1117809170519961703/1124055117562396763/uajY0XrMkYM-removebg-preview.png",
-      name: "Game house",
+      name: "Game House",
       description: "Discord türkiyenin en iyi oyun ortamı",
-      link: "https://discord.gg/gamehouse",
+      link: "https://discord.gg/Game House",
+    },
+    {
+      id: 2,
+      image: "https://cdn.discordapp.com/attachments/1117809170519961703/1124065680627613796/JPEG_20180902_205240.jpg",
+      name: "Youtube Channel",
+      description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
+      link: "https://www.youtube.com/@irian35",
     }
   ];
-  res.status(200).json(data)
+  res.status(200).json(data);
+};
